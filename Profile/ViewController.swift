25622854a9ff7da89697a850cd0e24ea2aa68002
Saki Igeta <p19026is@gm.tsuda.ac.jp>
2021-05-09 23:37:06 +0900

    Initial Commit

---
+++ Profile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Profile
+//
+//  Created by 井ケ田　沙紀 on 2021/05/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
